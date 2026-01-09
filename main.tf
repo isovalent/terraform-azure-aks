a0eccddd59c5f97c9f138b3676fd0437f6ce76ea
Nick Bradford <nibradfo@cisco.com>
2026-01-09 16:15:16 +1100

    revert to 10.1.1 based patch

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ data "azuread_group" "admins" {
 
 // Create an AKS cluster.
 module "main" {
-  source  = "git::ssh://git@github.com/nmbradford/terraform-azurerm-aks.git?ref=main"
+  source  = "git::ssh://git@github.com/nmbradford/terraform-azurerm-aks.git?ref=10.1.1_custom_ca"
 
   agents_availability_zones            = sort(flatten(jsondecode(module.availability_zones_data_source.stdout)))
   location                             = var.region
